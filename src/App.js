e9902acddce31969074c3d3c11c8bd6ca7b30fce
Aditya77kushwaha <adityakushwaha2912@gmail.com>
2021-02-12 18:20:43 +0530

    Home page complete

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
     <Router>
       <div className="app">
         <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
           <Route path="/">
             <Header />
             <Home />
