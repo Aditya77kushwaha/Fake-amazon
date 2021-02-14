f6b7dc0e1a488d375ba7d30688a733cfab3f83db
Aditya77kushwaha <adityakushwaha2912@gmail.com>
2021-02-15 00:34:49 +0530

    Creates login/signup and auth

---
+++ src/Login.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
+import { auth } from "./firebase";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = e => {
+    e.preventDefault();
+  };
+
+  const register = e => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(auth => {
+        console.log(auth); //successfully created a new user
+      })
+      .catch(error => alert(error.message));
+  };
+
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+        />
+      </Link>
+
+      <div className="login__container">
+        <h1>Sign-in</h1>
+
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+
+          <button
+            type="submit"
+            className="login__signInButton"
+            onClick={signIn}
+          >
+            Sign In
+          </button>
+        </form>
+
+        <p>
+          By signing-in you agree to Amazon Clone's Conditions of Use & Sale.
+        </p>
+        <button onClick={register} className="login__registerButton">
+          Create your account
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCiAtmTWvh2TvHn736sPi1hncIjB3HFUdo",
@@ -8,3 +10,10 @@ const firebaseConfig = {
   appId: "1:747521478649:web:5b67ac6ac4c35d61d9f728",
   measurementId: "G-83BW1R1CRQ"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
